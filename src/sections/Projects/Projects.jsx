c1390a23284a583a7a2e3cc7069843b222731a1f
jierredansby <70708418+jierredansby@users.noreply.github.com>
2025-09-03 22:14:06 -0400

    done

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,14 +2,32 @@ import React from 'react'
 import styles from './ProjectsStyles.module.css'
 
 function Projects() {
-  return (
-    <section id = "projects" className={styles.container}>
-        <h1 className="sectionTitle">Projects</h1>
-        <div className= {styles.projectsContainer}>
-            <p>https://carsong.netlify.app/</p>
-            <hr />
+   return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+
+        <div className={styles.projectCard}>
+          <h2>This Website</h2>
+          <p>My personal portfolio built with React</p>
         </div>
-        </section>
+
+        <div className={styles.projectCard}>
+          <a href="https://edufd.netlify.app/" target="_blank" rel="noopener noreferrer">
+            <h2>EduFD</h2>
+            <p>A project I built focused on education-related web-design. Built with only JavaScript, HTML, and CSS.</p>
+          </a>
+        </div>
+
+        <div className={styles.projectCard}>
+          <a href="https://carsong.netlify.app/" target="_blank" rel="noopener noreferrer">
+            <h2>CarSong</h2>
+            <p>A portfolio project I had built for an old colleague of mine. Built with only JavaScript, HTML, and CSS.</p>
+          </a>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
