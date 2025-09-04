9f4f773849c58731c40b24b7b447d69c73b069fb
jierredansby <70708418+jierredansby@users.noreply.github.com>
2025-09-03 22:01:16 -0400

    done

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
     <section id = "projects" className={styles.container}>
         <h1 className="sectionTitle">Projects</h1>
         <div className= {styles.projectsContainer}>
-            <p>Coming Soon!</p>
+            <p>https://carsong.netlify.app/</p>
             <hr />
         </div>
         </section>
